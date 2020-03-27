cc532b1aadddf86448e15a586820f1a0098d974a
bouddhaacelee <bouddhaacelee@gmail.com>
2020-03-27 13:41:58 +0100

    unroll cast sbox

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,5 +1,8 @@
 using BenchmarkTools
 using DataEncryptionStandard
+msg = randomUInt64toBits()
+key = randomUInt64toBits()
+compute(msg, key)
 
 b = @benchmarkable compute(msg, key) setup=(msg = randomUInt64toBits(); key = randomUInt64toBits())
 tune!(b)
